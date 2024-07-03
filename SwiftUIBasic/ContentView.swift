490a36ced0557c3137316189f8d741ad7c9d6ca2
ninanee <ni.yun@outlook.com>
2024-07-03 14:20:16 -0400

    Initial Commit

---
+++ SwiftUIBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBasic
+//
+//  Created by Yun Ni on 2024-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by Yun Ni on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
